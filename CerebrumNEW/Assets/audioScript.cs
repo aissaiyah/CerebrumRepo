89bcb3e192dc891f6b63fab37936affe652255e8
aissaiyah <aissaiyah.bennett.sia@gmail.com>
2026-02-19 11:03:29 -0500

    New Scene Demo

---
+++ CerebrumNEW/Assets/audioScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cerebrum;
+
+public class audioScript : MonoBehaviour
+{
+    public bool OxygenOn = false;
+    public GameObject coughing;  // assign in Inspector
+
+    void Update()
+    {
+
+    }
+}
